73456ff8ed65cc1c7fdbee092c60cf667273ae39
asm-shaikat <asmshaikat1952@gmail.com>
2023-12-11 11:35:17 +0600

    Profile picture  validationa added

--- public/js/profile-image.js
+++ public/js/profile-image.js
@@ -1,26 +1,39 @@
-$(document).ready(function() {
-    $('#proimage').submit(function(event) {
-      // Prevent form from submitting automatically
-      event.preventDefault();
+function profilePhotoValidation() {
+    var fileInput = document.getElementById("profilePhoto");
+    var fileSize = fileInput.files[0].size; // in bytes
+    var maxSize = 2 * 1024 * 1024; // 2 MB
 
-      // Do some validation checks here
-      // ...
+    if (fileSize > maxSize) {
+        document.getElementById("profileValidationText").innerHTML =
+            "File size exceeds 2 MB. Please choose a smaller file.";
+        // Clear the file input to allow the user to choose another file
+        fileInput.value = "";
+    }
+}
 
-      // Submit the form using AJAX instead
-      $.ajax({
-        type: 'POST',
-        url: $('#proimage').attr('action'),
-        data: new FormData($('#proimage')[0]),
-        processData: false,
-        contentType: false,
-        success: function(response) {
-          // Handle the response from the server
-          // ...
-        },
-        error: function(xhr, status, error) {
-          // Handle any errors that occur during the AJAX request
-          // ...
-        }
-      });
+$(document).ready(function () {
+    $("#proimage").submit(function (event) {
+        // Prevent form from submitting automatically
+        event.preventDefault();
+
+        // Do some validation checks here
+        // ...
+
+        // Submit the form using AJAX instead
+        $.ajax({
+            type: "POST",
+            url: $("#proimage").attr("action"),
+            data: new FormData($("#proimage")[0]),
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                // Handle the response from the server
+                // ...
+            },
+            error: function (xhr, status, error) {
+                // Handle any errors that occur during the AJAX request
+                // ...
+            },
+        });
     });
-  });
+});
--- resources/views/profile/profile-photo-change.blade.php
+++ resources/views/profile/profile-photo-change.blade.php
@@ -26,13 +26,15 @@
                 </div>
                 <label class="block">
                     <span class="sr-only">Choose profile photo</span>
-                    <input type="file" name="profile_picture" class="block w-full text-sm text-slate-500
+                    <input type="file" onchange="profilePhotoValidation()" id="profilePhoto"
+                    name="profile_picture" class="block w-full text-sm text-slate-500
                     file:mr-4 file:py-2 file:px-4
                     file:rounded-full file:border-0
                     file:text-sm file:font-semibold
                     file:bg-violet-50 file:text-violet-700
                     hover:file:bg-violet-100
                     " />
+                    <small id="profileValidationText" class="text-red-600"></small>
                 </label>
             </div>
 
