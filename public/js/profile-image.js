c5f0e9f3ca614d8454c72cdc9bc44e1f9d28bf50
asm-shaikat <asmshaikat1952@gmail.com>
2023-12-11 11:43:16 +0600

    cover image validation

--- public/js/profile-image.js
+++ public/js/profile-image.js
@@ -11,6 +11,19 @@ function profilePhotoValidation() {
     }
 }
 
+function coverPhotoValidation() {
+  var fileInput = document.getElementById("coverPhoto");
+  var fileSize = fileInput.files[0].size; // in bytes
+  var maxSize = 2 * 1024 * 1024; // 2 MB
+
+  if (fileSize > maxSize) {
+      document.getElementById("coverPhotoValidationText").innerHTML =
+          "File size exceeds 2 MB. Please choose a smaller file.";
+      // Clear the file input to allow the user to choose another file
+      fileInput.value = "";
+  }
+}
+
 $(document).ready(function () {
     $("#proimage").submit(function (event) {
         // Prevent form from submitting automatically
--- resources/views/profile/cover-photo-change.blade.php
+++ resources/views/profile/cover-photo-change.blade.php
@@ -9,20 +9,22 @@
         <!-- cover image and profile photo -->
         <form  action="{{ route('profile.image-upload') }}" method="post" enctype="multipart/form-data">
             @csrf
-            <p class="text-lg text-black m-4">Cover Image</p>
+            <p class="text-lg text-black dark:text-white m-4">Cover Image</p>
             <div class="flex">
                 <div class="shrink-0">
                     <img class="h-16 w-16 object-cover mr-10" src="{{ $UserData->cover_picture }}" alt="Current profile photo" />
                 </div>
                 <label class="block">
                     <span class="sr-only">Choose cover photo</span>
-                    <input type="file" name="cover_picture" class="block w-full text-sm text-slate-500
+                    <input type="file" onchange="coverPhotoValidation()" id="coverPhoto" 
+                    name="cover_picture" class="block w-full text-sm text-slate-500
                     file:mr-4 file:py-2 file:px-4
                     file:rounded-full file:border-0
                     file:text-sm file:font-semibold
                     file:bg-violet-50 file:text-violet-700
                     hover:file:bg-violet-100
                     " />
+                    <small id="coverPhotoValidationText" class="text-red-600"></small>
                 </label>
             </div>
             <button type="submit" class="mt-10 btn btn-success">Save</button>
