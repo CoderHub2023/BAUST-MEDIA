adea767830506d2bc82feb3a199c9b9c0af8f364
asm-shaikat <asmshaikat1952@gmail.com>
2023-12-17 16:14:31 +0600

    remove active people from comment module

--- resources/views/comments.blade.php
+++ resources/views/comments.blade.php
@@ -178,56 +178,7 @@
 
 
 
-<div class="w-1/6 h-screen bg-gray-800 p-4 text-white hidden md:block">
-  <h2 class="text-2xl font-semibold mb-4">Navigation</h2>
-  <ul class="space-y-2 overflow-y-auto" style="max-height: 60vh;"> <!-- Adjust max-height as needed -->
-    <li class="flex items-center space-x-2 p-2">
-      <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
-        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-        <path fill-rule="evenodd" d="M10 0a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 100-16 8 8 0 000 16z" clip-rule="evenodd" />
-      </svg>
-      <a href="#" class="block hover:text-blue-500">Item 1</a>
-    </li>
-    <li class="flex items-center space-x-2 p-2">
-      <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
-        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-        <path fill-rule="evenodd" d="M10 0a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 100-16 8 8 0 000 16z" clip-rule="evenodd" />
-      </svg>
-      <a href="#" class="block hover:text-blue-500">Item 2</a>
-    </li>
-    <!-- Add more items as needed -->
-  </ul>
 
-  <!-- Active People -->
-  <div class="mt-4">
-    <h3 class="text-lg font-semibold mb-2">Active People</h3>
-    <ul class="space-y-2">
-      <li class="flex items-center space-x-2 p-2">
-        <!-- Green dot to indicate active -->
-        <span class="w-3 h-3 bg-green-500 rounded-full"></span>
-        <!-- Active person's avatar using Tailwind Avatar component -->
-        <div class="avatar">
-          <div class="w-5 h-5 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src="{{ $loggedInUserData[0]->profile_picture }}" />
-          </div>
-        </div>
-        <span class="block text-sm">Active Person 1</span>
-      </li>
-      <li class="flex items-center space-x-2 p-2">
-        <!-- No green dot (not active) -->
-        <span class="w-3 h-3 bg-gray-400 rounded-full"></span>
-        <!-- Active person's avatar using Tailwind Avatar component -->
-        <div class="avatar">
-          <div class="w-5 h-5 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src="{{ $loggedInUserData[0]->profile_picture }}" />
-          </div>
-        </div>
-        <span class="block text-sm">Active Person 2</span>
-      </li>
-      <!-- Add more active people as needed -->
-    </ul>
-  </div>
-</div>
 </div>
 <script>
   // Function for like a Stack
