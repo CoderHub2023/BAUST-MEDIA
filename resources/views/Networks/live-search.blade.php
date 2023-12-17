29614e20a4aae7f2e7f5d63cc14024586c835fb1
asm-shaikat <asmshaikat1952@gmail.com>
2023-12-18 02:45:22 +0600

    searching module

--- resources/views/Networks/live-search.blade.php
+++ resources/views/Networks/live-search.blade.php
@@ -1,19 +1,19 @@
 @extends('template')
 
-@section('title', 'Network')
+@section('title', 'Search')
 @include('layouts.public-nav')
 
 @section('content')
 <div class="w-full flex">
     <div class="w-1/6 h-screen  p-4 text-white hidden md:block shadow-lg">
         <ul class="space-y-2 overflow-y-auto" style="max-height: 80vh;">
-            <a href="/my-network" class="block hover:text-blue-500 bg-slate-400 rounded-lg">
+            <a href="/my-network" class="block hover:text-blue-500">
                 <li class="flex items-center space-x-2 p-2">
                     <p class="text-sm p-2 text-black dark:text-white">Home</p>
                 </li>
             </a>
 
-            <a href="/search-network" class="block hover:text-blue-500 rounded-lg">
+            <a href="/search-network" class="block hover:text-blue-500 bg-slate-400">
                 <li class="flex items-center space-x-2 p-2">
                     <p class="text-sm p-2 text-black dark:text-white">Search</p>
                 </li>
@@ -26,23 +26,21 @@
             </a>
 
             <a href="/followers" class="block hover:text-blue-500">
-                <li class="flex items-center space-x-2 p-2">
+                <li class="flex items-center space-x-2 p-2 rounded-lg">
                     <p class="text-sm p-2 text-black dark:text-white">Followers</p>
                 </li>
             </a>
         </ul>
     </div>
-
-
+    <!-- Mobile bar -->
     <div class="xl:w-3/4 2xl:w-4/5 w-full mx-auto">
         <div class="px-4 sm:px-6 md:px-8 lg:px-10 py-4 md:py-7">
             <div class="flex flex-wrap items-center justify-between">
                 <div>
-                    <p tabindex="0" class="focus:outline-none text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-black  dark:text-white w-full sm:w-auto">Connect With People</p>
                     <div class=" lg:hidden xl:hidden 2xl:hidden text-sm breadcrumbs text-black">
                         <ul>
                             <li>
-                                <a href="/my-network" class="underline">
+                                <a href="/my-network">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 stroke-current">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
                                     </svg>
@@ -50,7 +48,7 @@
                                 </a>
                             </li>
                             <li>
-                                <a href="/search-network">
+                                <a href="/search-network" class="underline">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-4 h-4 stroke-current">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
                                     </svg>
@@ -79,34 +77,58 @@
                     </div>
                 </div>
                 <div>
-                    {{ $usersNotInNetwork->links() }}
+                    <!-- Pagination -->
                 </div>
             </div>
         </div>
+        <div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 ml-10">
-            @foreach($usersNotInNetwork as $user)
-            <div class="max-w-xs bg-white shadow-lg rounded-md overflow-hidden">
-                <div class="p-3">
-                    <img src="{{ $user->profile_picture }}" alt="Current profile photo" class="w-full h-16vh mb-2 rounded-md">
-                    <p class="font-semibold text-gray-800 text-sm">{{ $user->name }}</p>
-                    <p class="text-gray-600 text-xs mb-2">{{ $user->headlines }}</p>
+        </div>
 
-                    <div class="p-2 border-t border-gray-200">
-                        <a href="{{ url('/add-network/'.$user->id) }}" class="btn btn-sm btn-secondary">Follow</a>
-                    </div>
-                </div>
+        <div>
+            <form action="{{ route('searching') }}" method="post">
+                @csrf
+                <p class="text-black dark:text-white text-center">Search</p>
+                <input id="searchInput" name="query" class="px-4 py-2 dark:bg-gray-700 bg-slate-400 border rounded-l-md dark:text-white text-black placeholder:text-black focus:outline-none" type="text" placeholder="Search..." />
+                <button id="searchButton" type="submit" class="px-4 py-2 bg-sky-600 dark:bg-gray-600 border rounded-r-md dark:text-white text-black hover:bg-gray-700 focus:outline-none">Search</button>
+            </form>
+            @if(!$users->total() > 0)
+            <div>
+                <p class="text-red-700">No result found</p>
             </div>
-            @endforeach
+            @else
+            <div class="bg-white dark:bg-gray-900 px-4 sm:px-6 md:px-8 pb-5">
+            <div class="overflow-x-auto">
+                <table class="w-full whitespace-nowrap">
+                    <tbody>
+                        <!-- Row start -->
+                        @foreach($users as $user)
+                        <tr tabindex="0" class="focus:outline-none text-sm leading-none text-gray-600 dark:text-gray-200 h-16">
+                            <td class="w-full sm:w-1/2">
+                                <div class="flex items-center">
+                                    <div class="w-20 h-20 sm:w-40 sm:h-20 flex items-center justify-center">
+                                        <img class="h-20 w-20 sm:w-40 object-cover mr-4 sm:mr-10" src="{{ $user->profile_picture }}" alt="Current profile photo" />
+                                    </div>
+                                    <div class="pl-2">
+                                        <p class="text-sm font-medium leading-none text-gray-800 dark:text-white">{{ $user->name }}</p>
+                                        <p class="text-xs leading-3 text-gray-600 dark:text-gray-200 mt-1 sm:mt-2">{{ $user->headlines }}</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="pl-4 sm:pl-16">
+                                <button type="submit" class="btn btn-sm btn-secondary"><a href="{{ url('/add-network/'.$user->id) }}">Follow</a></button>
+                                <!-- <button type="submit" class="btn btn-disabled btn-sm">Request Send</button> -->
+                            </td>
+                        </tr>
+                        @endforeach
+                        <!-- Row End -->
+                    </tbody>
+                </table>
+            </div>
         </div>
-
-        <div class="lg:hidden md:hidden 2xl:hidden">
-            {{ $usersNotInNetwork->links() }}
+        {{ $users->links() }}
         </div>
-
-
-
-
     </div>
+    @endif
 </div>
 @endsection
